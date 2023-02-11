d22bbc05457a2e1522eea963545f5b8a2e49947d
Abhinav Jha <abhinavrocx@gmail.com>
2023-02-11 18:44:58 +0530

    Added square function

--- test2/file1.cpp
+++ test2/file1.cpp
@@ -1,9 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int newfun(int n)
+{
+    return n * n;
+}
+
 int main()
 {
     cout << "Hello world";
     cout << 1 + 2 << endl;
+    cout << newfun(5) << endl;
     return 0;
 }
